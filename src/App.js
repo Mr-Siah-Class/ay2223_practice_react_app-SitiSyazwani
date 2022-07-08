02c6571d3d80d84b12ad808a66b794aa42c4aded
Wani Jusri <sitisyazwani.21@ichat.sp.edu.sg>
2022-07-08 15:27:31 +0800

    added single screen & edited navbar #12

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import HomeScreen from './screens/HomeScreen';
 import ProductsScreen from './screens/ProductsScreen';
 import NoScreen from './screens/NoScreen';
 import FooterBar from './components/Footerbar';
-// import SingleProductScreen from './screens/SingleProductScreen';
+import SingleProductScreen from './screens/SingleProductScreen';
 
 class App extends React.Component
 {
@@ -31,7 +31,7 @@ class App extends React.Component
                   <Routes>
                     <Route exact path="/" element={<HomeScreen/>}/>
                     <Route path="/products" element={<ProductsScreen/>}/>
-                    {/* <Route path="/products/:productId" element={<SingleProductScreen/>}/>  */}
+                    <Route path="/products/:productId" element={<SingleProductScreen/>}/> 
                     <Route path="*" element={<NoScreen/>}/>
                   </Routes>
                 </BrowserRouter>
